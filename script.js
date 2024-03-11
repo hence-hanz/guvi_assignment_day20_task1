50f43eb7a0f69f1c4596f7fce148187ad875ccd4
Haneesh p <116893066+hence-hanz@users.noreply.github.com>
2024-03-12 00:11:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//fetching data from api and display it on the page 
+function data(){
+    fetch("https://api.imgflip.com/get_memes").then(response=>{
+        if (!response.ok){
+            throw Error("error")
+        }
+      return response.json()
+    }).then(value => {
+        console.log(value.data.memes)
+        let data1 = ""
+         value.data.memes.map(ele=>{
+            data1 += `
+            <div class="card">
+            <h3 class="card-title">id : ${ele.id}</h3>
+            <h3 class="card-title">name : ${ele.name}</h3>
+            <img src=${ele.url} alt="img" class="img">
+            </div>
+            `
+        })
+         document.getElementById('cards').innerHTML=data1;
+    }).catch(error =>{
+        console.log(error)
+    })
+}
+data()
